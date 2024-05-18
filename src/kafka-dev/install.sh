0ca879f465950e471dd210c1a94c5ecf9adff25b
Josh Hollandsworth <142175+joshholl@users.noreply.github.com>
2024-05-17 20:12:04 -0400

    Use echo instead of apt-add-repo
    The base images doesnt have apt-add-repository, rather than instal it just echo the sources

--- src/kafka-dev/install.sh
+++ src/kafka-dev/install.sh
@@ -9,7 +9,8 @@ if [ "${ID}" != "debian" ] && [ "${ID_LIKE}" != "debian" ]; then
   exit 1
 fi
 
-wget -qO - https://packages.confluent.io/deb/${CONFLUENT_PLATFORM_VERSION}/archive.key | sudo apt-key add -
-sudo add-apt-repository "deb https://packages.confluent.io/deb/${CONFLUENT_PLATFORM_VERSION} stable main"
-sudo add-apt-repository "deb https://packages.confluent.io/clients/deb $(lsb_release -cs) main"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://packages.confluent.io/deb/${CONFLUENT_PLATFORM_VERSION}/archive.key  | gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+sudo echo "deb https://packages.confluent.io/deb/${CONFLUENT_PLATFORM_VERSION} stable main" | tee /etc/apt/sources.list.d/confluent.list
+sudo echo "deb https://packages.confluent.io/clients/deb $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/confluent_client.list
 sudo apt-get update && sudo apt-get install confluent-community kcat
