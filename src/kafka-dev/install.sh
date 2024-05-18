49da309a502193b6d1b27b786950b38c1ddafcab
Josh Hollandsworth <142175+joshholl@users.noreply.github.com>
2024-05-17 20:58:12 -0400

    fixing things use commands that the dev container already has

--- src/kafka-dev/install.sh
+++ src/kafka-dev/install.sh
@@ -9,8 +9,10 @@ if [ "${ID}" != "debian" ] && [ "${ID_LIKE}" != "debian" ]; then
   exit 1
 fi
 
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://packages.confluent.io/deb/${CONFLUENT_PLATFORM_VERSION}/archive.key  | gpg --dearmor -o /etc/apt/keyrings/charm.gpg
-sudo echo "deb https://packages.confluent.io/deb/${CONFLUENT_PLATFORM_VERSION} stable main" | tee /etc/apt/sources.list.d/confluent.list
-sudo echo "deb https://packages.confluent.io/clients/deb $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/confluent_client.list
-sudo apt-get update && sudo apt-get install confluent-community kcat
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://packages.confluent.io/deb/${CONFLUENT_PLATFORM_VERSION}/archive.key  | sudo gpg --dearmor -o /etc/apt/keyrings/confluent.gpg
+sudo echo "deb [signed-by=/etc/apt/keyrings/confluent.gpg] https://packages.confluent.io/deb/${CONFLUENT_PLATFORM_VERSION} stable main" | sudo tee /etc/apt/sources.list.d/confluent.list
+sudo echo "deb [signed-by=/etc/apt/keyrings/confluent.gpg] https://packages.confluent.io/clients/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/confluent_client.list
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update && sudo apt-get -y install confluent-community-* kcat
